30d55a9f9ac9e503ccda23c14f0b31454bc4edd6
Shelley Dogra <shelleycdogra@lewisu.edu>
2021-03-02 11:00:07 -0600

    ML 2

--- source/js/script.js
+++ source/js/script.js
@@ -6,3 +6,15 @@ video.playbackRate = .55;
 
 //Constants
 
+//nav
+const btnHamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('click', function() {
+    console.log('click hamberger');
+
+    if(btnHamburger.classList.contains('open')) {
+        btnHamburger.classList.remove('open');
+    } else {
+        btnHamburger.classList.add('open');
+    }
+})
