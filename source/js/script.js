c0e868ec62e729e0ab31475d9ed52973ec85e73d
Shelley Dogra <shelleycdogra@lewisu.edu>
2021-03-04 16:26:36 -0600

    ML - nonmobile view todo

--- source/js/script.js
+++ source/js/script.js
@@ -8,13 +8,21 @@ video.playbackRate = .55;
 
 //nav
 const btnHamburger = document.querySelector('#btnHamburger');
+const overlay = document.querySelector('#overlay');
 
 btnHamburger.addEventListener('click', function() {
     console.log('click hamberger');
 
     if(btnHamburger.classList.contains('open')) {
         btnHamburger.classList.remove('open');
+        overlay.classList.add('hidden');
     } else {
         btnHamburger.classList.add('open');
+        overlay.classList.remove('hidden');
     }
+})
+
+overlay.addEventListener('click', () => {
+    overlay.classList.add('hidden');
+    btnHamburger.classList.remove('open');
 })
